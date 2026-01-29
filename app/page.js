dd4b60fa9fa1c6a8e9314b1a597e0838691d936b
Isaac Renan <135731056+IsaacRenan@users.noreply.github.com>
2026-01-29 17:21:57 -0300

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <body class="bg-white">
+    <body class="bg-red-500">
       <div className="bg-white p-20 rounded-lg mb-8">
         <h1 className="text-black">ola</h1>
       </div>
 
-      <div className="bg-pink-300 p-20 mb-8 items-center justify-center flex rounded-lg">
+      <div className="bg-red-500 p-20 mb-8 items-center justify-center flex rounded-lg">
         <div className="bg-green-300 w-90 h-80 flex m-10 border-2 rounded-lg">
               <div className="bg-gray-500 w-90 h-50 flex m-0 border-0 rounded-lg">
                 <Image
